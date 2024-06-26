a3e4d9ade277a451ee085ec8da44a36303391911
Adnan Istiaque <108503483+shelby2770@users.noreply.github.com>
2024-06-26 19:17:30 +0600

    upating assignment 1

--- 23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.py
+++ 23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.py
@@ -1,30 +1,24 @@
 import heapq
 
-f = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.txt")
+input_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.txt")
+output_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_OutputFile_Summer2024.txt", "w")
+adj = {}
+h = {}
 
-dict = {}
-dict2 = {}
-for i, j in enumerate(f):
-    dict[j.split()[0]] = i
-    dict2[i] = j.split()[0]
-f.seek(0)
-
-n = len(dict)
-adj = [[] for i in range(n)]
-h = [None for i in range(n)]
-
-for i, j in enumerate(f):
-    lst = j.split()
-    h[i] = int(lst[1])
+for i in input_file:
+    lst = i.split()
+    now = lst[0]
+    h[now] = int(lst[1])
+    adj[now] = []
     for k in range(2, len(lst), 2):
-        adj[i].append((dict[lst[k]], int(lst[k + 1])))
+        adj[now].append((lst[k], int(lst[k + 1])))
 
-s = dict[input("Start node: ")]
-d = dict[input("Destination: ")]
-pq = []
-vis = [float('inf') for i in range(n)]
-par = [None for i in range(n)]
+s = input("Start node: ")
+d = input("Destination: ")
+vis = {key: float('inf') for key in adj.keys()}
+par = {key: None for key in adj.keys()}
 vis[s] = h[s]
+pq = []
 heapq.heappush(pq, (h[s], s))
 while len(pq):
     val, now = heapq.heappop(pq)
@@ -38,7 +32,7 @@ while len(pq):
             heapq.heappush(pq, (vis[i[0]], i[0]))
 
 if vis[d] == float('inf'):
-    print("NO PATH FOUND")
+    output_file.write("NO PATH FOUND\n")
 else:
     path = [d]
     dist = 0
@@ -47,7 +41,10 @@ else:
         path.append(par[now][0])
         dist += par[now][1]
         now = par[now][0]
-    print("Path: ", end='')
+    output_file.write("Path: ")
     for i in range(len(path) - 1, -1, -1):
-        print(dict2[path[i]], end=" -> " if i else "\n")
-    print("Total distance:", dist, "km")
+        output_file.write(f"{path[i]}{" -> " if i else "\n"}")
+    output_file.write(f"Total distance: {dist} km")
+
+input_file.close()
+output_file.close()
