f07772312f2dce23810df1c414234c5933c70ad8
Adnan Istiaque <108503483+shelby2770@users.noreply.github.com>
2024-07-05 20:40:37 +0600

    part 2 done

--- 23341065_Istiaque_CSE422_08_Lab_Assignment02_Summer2024.py
+++ 23341065_Istiaque_CSE422_08_Lab_Assignment02_Summer2024.py
@@ -23,8 +23,8 @@ def fitness(s):
     return -overlap_penalty - consistency_penalty
 
 
-def selection():
-    return population[random.randint(0, len(population) - 1)], population[random.randint(0, len(population) - 1)]
+def selection(n):
+    return population[random.randint(0, n - 1)], population[random.randint(0, n - 1)]
 
 
 def crossover(parent1, parent2):
@@ -37,20 +37,35 @@ def mutate(s):
 
 
 population = [''.join(random.choices("01", k=n * t)) for i in range(10)]
+tmp = population  # soterd initial population for later part
 population.sort(key=lambda i: -fitness(i))
 
+# part 1
 for i in range(10):
     if fitness(population[0]) == 0:
         break
     next_gen = []
     for j in range(10):
-        parent1, parent2 = selection()
-        child1, child2 = crossover(parent1, parent2), crossover(parent2, parent1)
-        next_gen.append(mutate(child1))
-        next_gen.append(mutate(child2))
+        parent1, parent2 = selection(len(population))
+        offspring1, offspring2 = crossover(parent1, parent2), crossover(parent2, parent1)
+        next_gen.append(mutate(offspring1))
+        next_gen.append(mutate(offspring2))
     next_gen.sort(key=lambda i: -fitness(i))
     population = next_gen
-output_file.write(f"{population[0]}\n{fitness(population[0])}")
+output_file.write(f"{population[0]}\n{fitness(population[0])}\n")
+
+
+# part 2
+def two_point_crossover(parent1, parent2):
+    point1 = random.randint(1, len(parent1) - 2)
+    point2 = random.randint(point1, len(parent1) - 1)
+    return (parent1[:point1] + parent2[point1:point2] + parent1[point2:],
+            parent2[:point1] + parent1[point1:point2] + parent2[point2:])
+
+
+parent1, parent2 = random.choices(tmp, k=2)
+offspring1, offspring2 = two_point_crossover(parent1, parent2)
+output_file.write(f"Parent 1: {parent1}\nParent 2: {parent2}\nOffspring 1: {offspring1}\nOffspring 2: {offspring2}\n")
 
 input_file.close()
 output_file.close()
--- Lab Assignment 1/23341065_Istiaque_CSE422_08_Lab_Assignment01_Summer2024.py
+++ Lab Assignment 1/23341065_Istiaque_CSE422_08_Lab_Assignment01_Summer2024.py
@@ -1,51 +1,51 @@
-# import heapq
-#
-# input_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.txt")
-# output_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_OutputFile_Summer2024.txt", "w")
-# adj = {}
-# h = {}
-#
-# for i in input_file:
-#     lst = i.split()
-#     now = lst[0]
-#     h[now] = int(lst[1])
-#     adj[now] = []
-#     for k in range(2, len(lst), 2):
-#         adj[now].append((lst[k], int(lst[k + 1])))
-#
-# s = input("Start node: ")
-# d = input("Destination: ")
-# vis = {key: float('inf') for key in adj.keys()}
-# par = {key: None for key in adj.keys()}
-# vis[s] = h[s]
-# pq = []
-# heapq.heappush(pq, (h[s], s))
-# while len(pq):
-#     val, now = heapq.heappop(pq)
-#     if vis[now] != val: continue
-#     if now == d: break
-#     val -= h[now]
-#     for i in adj[now]:
-#         if val + i[1] + h[i[0]] < vis[i[0]]:
-#             vis[i[0]] = val + i[1] + h[i[0]]
-#             par[i[0]] = (now, i[1])
-#             heapq.heappush(pq, (vis[i[0]], i[0]))
-#
-# if vis[d] == float('inf'):
-#     output_file.write("NO PATH FOUND\n")
-# else:
-#     path = [d]
-#     dist = 0
-#     now = d
-#     while par[now]:
-#         path.append(par[now][0])
-#         dist += par[now][1]
-#         now = par[now][0]
-#     output_file.write("Path: ")
-#     for i in range(len(path) - 1, -1, -1):
-#         output_file.write(f"{path[i]}{" -> " if i else "\n"}")
-#     output_file.write(f"Total distance: {dist} km")
-#
-# input_file.close()
-# output_file.close()
+import heapq
+
+input_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_InputFile_Summer2024.txt")
+output_file = open("23341065_Istiaque_CSE422_08_Lab_Assignment01_OutputFile_Summer2024.txt", "w")
+adj = {}
+h = {}
+
+for i in input_file:
+    lst = i.split()
+    now = lst[0]
+    h[now] = int(lst[1])
+    adj[now] = []
+    for k in range(2, len(lst), 2):
+        adj[now].append((lst[k], int(lst[k + 1])))
+
+s = input("Start node: ")
+d = input("Destination: ")
+vis = {key: float('inf') for key in adj.keys()}
+par = {key: None for key in adj.keys()}
+vis[s] = h[s]
+pq = []
+heapq.heappush(pq, (h[s], s))
+while len(pq):
+    val, now = heapq.heappop(pq)
+    if vis[now] != val: continue
+    if now == d: break
+    val -= h[now]
+    for i in adj[now]:
+        if val + i[1] + h[i[0]] < vis[i[0]]:
+            vis[i[0]] = val + i[1] + h[i[0]]
+            par[i[0]] = (now, i[1])
+            heapq.heappush(pq, (vis[i[0]], i[0]))
+
+if vis[d] == float('inf'):
+    output_file.write("NO PATH FOUND\n")
+else:
+    path = [d]
+    dist = 0
+    now = d
+    while par[now]:
+        path.append(par[now][0])
+        dist += par[now][1]
+        now = par[now][0]
+    output_file.write("Path: ")
+    for i in range(len(path) - 1, -1, -1):
+        output_file.write(f"{path[i]}{" -> " if i else "\n"}")
+    output_file.write(f"Total distance: {dist} km")
+
+input_file.close()
+output_file.close()
 
